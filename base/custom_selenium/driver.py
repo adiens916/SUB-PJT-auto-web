4c41dc89f3129fcad5e200c02db077b944596421
Inho <adiens916@naver.com>
2024-05-18 09:56:26 +0900

    [refactor] Split custom option

--- base/custom_selenium/driver.py
+++ base/custom_selenium/driver.py
@@ -1,22 +1,9 @@
-from selenium import webdriver
-
 from driver_aux.custom_driver import CustomWebDriver
+from driver_aux.custom_option import CustomOption
 
 
 class Driver:
     @classmethod
     def get_chrome_driver(cls):
-        # driver = webdriver.Chrome(options=cls.__get_options(cls))
-        driver = CustomWebDriver(options=cls.__get_options(cls))
+        driver = CustomWebDriver(options=CustomOption())
         return driver
-
-    def __get_options(self):
-        # 크롬 드라이버 옵션 설정
-        arguments = ["--no-first-run", "--no-service-autorun", "--password-store=basic"]
-
-        options = webdriver.ChromeOptions()
-        options.add_argument(" ".join(arguments))
-        options.page_load_strategy = (
-            "none"  # 페이지가 전부 로딩되기 전에도 execute_script 가능
-        )
-        return options
