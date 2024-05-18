7ae8e00262bb9cbdf8dfcdd29cc06b66b7fdbaaf
Inho <adiens916@naver.com>
2024-05-18 09:12:31 +0900

    [refactor] Split custom web element

--- base/custom_selenium/driver.py
+++ base/custom_selenium/driver.py
@@ -1,27 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 
-
-class CustomWebElement(WebElement):
-    def click_by_script(self) -> None:
-        self.parent: webdriver.Chrome
-        self.parent.execute_script("arguments[0].click();", self)
-
-    def clear_by_script(self) -> None:
-        self.parent: webdriver.Chrome
-        self.parent.execute_script("arguments[0].value='';", self)
-
-    def send_keys_by_script(self, value) -> None:
-        self.parent: webdriver.Chrome
-        self.parent.execute_script(f"arguments[0].value='{value}';", self)
-
-    def scroll_into_view(self) -> None:
-        self.parent: webdriver.Chrome
-        self.parent.execute_script("arguments[0].scrollIntoView(true);", self)
-
-    def find_element(self, locator: tuple[str, str]) -> "CustomWebElement":
-        return super().find_element(by=locator[0], value=locator[1])
+from driver_aux.custom_element import CustomWebElement
 
 
 class CustomWebDriver(webdriver.Chrome):
@@ -44,5 +24,7 @@ class Driver:
 
         options = webdriver.ChromeOptions()
         options.add_argument(" ".join(arguments))
-        options.page_load_strategy = "none"  # 페이지가 전부 로딩되기 전에도 execute_script 가능
+        options.page_load_strategy = (
+            "none"  # 페이지가 전부 로딩되기 전에도 execute_script 가능
+        )
         return options
